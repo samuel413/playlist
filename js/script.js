570c4c8420e2b74ab48eae1e8544dee559eec561
samuel413 <sapereira@s.sfusd.edu>
2020-01-07 02:21:18 +0000

    make the goddamn function and everything else is free

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let songName = ['Pokemon Typing Adventure Boss Theme', 'Satori Maiden ~ 3rd Eye', 'Masked Dedede Theme', 'Hardware Store', 'Magnus von Grapple Theme', 'One Who Gets In Our Way', 'Battle on the Big Bridge', 'Faith is for the Transient People', 'Trap Factory', 'Skull Man Stage', 'Beauty is a Mad Mistress', 'Horsechestra STRONG Version'];
+let songArtist = [];
+let songLength = [];
+let imageUrl = [];
+let songUrl = [];
+
+function addSong (name, artist, length, imgurl, songurl) {
+    //add to the front of the array and call first value in each array?
+}
