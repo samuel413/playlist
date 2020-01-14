c3aecf1295c8819d2f626e296b8dcee5ab405fb4
samuel413 <sapereira@s.sfusd.edu>
2020-01-14 02:21:08 +0000

    need to debug the huge goddamn function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 let songName = ['Pokemon Typing Adventure Boss Theme', 'Satori Maiden ~ 3rd Eye', 'Masked Dedede Theme', 'Hardware Store', 'Magnus von Grapple Theme', 'One Who Gets In Our Way', 'Battle on the Big Bridge', 'Faith is for the Transient People', 'Trap Factory', 'Skull Man Stage', 'Beauty is a Mad Mistress', 'Horsechestra STRONG Version'];
 let songArtist = [];
 let songLength = [];
-let imageUrl = [];
 let songUrl = [];
 
-function addSong (name, artist, length, imgurl, songurl) {
-    //add to the front of the array and call first value in each array?
+function addSong () {
+    let song = $('.song').val();
+    let artist = $('.artist').val();
+    let length = $('.length').val();
+    let link = $('.link').val();
+    songName.push(song);
+    songArtist.push(artist);
+    songLength.push(length);
+    songUrl.push(link);
+    $(".s").empty();
+    $(".a").empty();
+    $(".le").empty();
+    $(".li").empty();
+    songName.forEach(function(song) {
+        $('.s').append(`<li>${song}</li>`);
+    })
+    songArtist.forEach(function(artist) {
+        $('.a').append(`<li>${artist}</li>`);
+    })
+    songLength.forEach(function(length) {
+        $('.le').append(`<li>${length}</li>`);
+    })
+    songUrl.forEach(function(link) {
+        $('.li').append(`<li><a href="${link}"></a></li>`);
+    })
 }
+
+$('.add').click(function() {
+    addSong();
+});
